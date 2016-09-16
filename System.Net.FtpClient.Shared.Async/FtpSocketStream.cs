774b812267aaaaa79a3ba2daaf6b67c85ff11f9c
Mark Junker <m.junker@fubar-dev.de>
2016-09-16 12:53:06 +0200

    First try for conversion to .NET Core

---
+++ System.Net.FtpClient.Shared.Async/FtpSocketStream.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace System.Net.FtpClient {
+    /// <summary>
+    /// Stream class used for talking. Used by FtpClient, extended by FtpDataStream
+    /// </summary>
+    public partial class FtpSocketStream {
+        /// <summary>
+        /// Asynchronously accepts a connection from a listening socket
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public IAsyncResult BeginAccept(AsyncCallback callback, object state) {
+            if (m_socket != null)
+                return m_socket.BeginAccept(callback, state);
+            return null;
+        }
+
+        /// <summary>
+        /// Completes a BeginAccept() operation
+        /// </summary>
+        /// <param name="ar">IAsyncResult returned from BeginAccept</param>
+        public void EndAccept(IAsyncResult ar) {
+            if (m_socket != null) {
+                m_socket = m_socket.EndAccept(ar);
+                m_netStream = new NetworkStream(m_socket);
+            }
+        }
+    }
+}
---
+++ System.Net.FtpClient.Shared.AsyncTask/FtpSocketStream.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace System.Net.FtpClient
+{
+    /// <summary>
+    /// Stream class used for talking. Used by FtpClient, extended by FtpDataStream
+    /// </summary>
+    public partial class FtpSocketStream {
+#if NET45
+        public async Task AcceptAsync() {
+            m_socket = await Task<Socket>.Factory.FromAsync(
+                                             (callback, state) => ((Socket) state).BeginAccept(callback, state),
+                                             asyncResult => ((Socket) asyncResult.AsyncState).EndAccept(asyncResult),
+                                             state: m_socket).ConfigureAwait(false);
+            m_netStream = new NetworkStream(m_socket);
+        }
+#else
+        public async Task AcceptAsync() {
+            m_socket = await m_socket.AcceptAsync().ConfigureAwait(false);
+            m_netStream = new NetworkStream(m_socket);
+        }
+#endif
+    }
+}
